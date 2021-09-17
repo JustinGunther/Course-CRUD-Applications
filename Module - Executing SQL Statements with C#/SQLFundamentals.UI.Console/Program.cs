8f8ffff0810a20eafcdf92cceda3d8c14ff24814
Justin Gunther <justin.gunther@nascentvisions.com>
2021-09-17 13:04:38 -0500

    Initial Project Setup

---
+++ Module - Executing SQL Statements with C#/SQLFundamentals.UI.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SQLFundamentals.UI.Console
+{
+    class Program
+    {
+
+        static void Main(string []args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+        
+    }
+}
+
