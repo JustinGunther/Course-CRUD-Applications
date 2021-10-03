1661228886b6c7945e992bcad64fa6ab8ccb6e5c
Shaun Borey <sborey@yahoo.com>
2021-10-03 03:59:16 -0400

    Added config file to hold connection string and modified code to obtain connection string from config file.  Added while loops to handle invalid option input.  Made minor spelling and formatting changes.

--- Module - Executing SQL Statements with C#/SQLFundamentals.UI.CRUDConsole/Program.cs
+++ Module - Executing SQL Statements with C#/SQLFundamentals.UI.CRUDConsole/Program.cs
@@ -9,171 +9,218 @@ namespace SQLFundamentals.UI.CRUDConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("CRUD Operations Demo Application");
             ChooseOption();
         }
 
         static void ChooseOption()
         {
-            Console.WriteLine("\n----------------------------------\n");
-            Console.WriteLine("Choose a Option:");
-            Console.WriteLine("\t1 - Press 1 to create a Contact");
-            Console.WriteLine("\t2 - Press 2 to update a Contact");
-            Console.WriteLine("\t3 - Press 3 to delete a Contact");
-            Console.WriteLine("\t4 - Press 4 to get all Contacts");
-            Console.WriteLine("\t5 - Press 5 to get a Contact by ID");
-            Console.WriteLine("\t(6) - Close Appliction");
-            Console.Write("Your choice: ");
-            string optionNo = Console.ReadLine();
-            switch (optionNo)
+            bool validOption = false;
+
+            while (!validOption)
             {
-                case "1":
-                    CreateContact();
-                    break;
-                case "2":
-                    UpdateContact();
-                    break;
-                case "3":
-                    DeleteContact();
-                    break;
-                case "4":
-                    GetAllContacts();
-                    break;
-                case "5":
-                    GetContactByID();
-                    break;
-                default:
-                    break;
+                Console.WriteLine("\n----------------------------------\n");
+                Console.WriteLine("Choose an Option:");
+                Console.WriteLine("\t1 - Press 1 to create a Contact");
+                Console.WriteLine("\t2 - Press 2 to update a Contact");
+                Console.WriteLine("\t3 - Press 3 to delete a Contact");
+                Console.WriteLine("\t4 - Press 4 to get all Contacts");
+                Console.WriteLine("\t5 - Press 5 to get a Contact by ID");
+                Console.WriteLine("\t6 - Close Appliction");
+                Console.Write("Your choice: ");
+                string optionNo = Console.ReadLine();
+                switch (optionNo)
+                {
+                    case "1":
+                        validOption = true;
+                        CreateContact();
+                        break;
+                    case "2":
+                        validOption = true;
+                        UpdateContact();
+                        break;
+                    case "3":
+                        validOption = true;
+                        DeleteContact();
+                        break;
+                    case "4":
+                        validOption = true;
+                        GetAllContacts();
+                        break;
+                    case "5":
+                        validOption = true;
+                        GetContactByID();
+                        break;
+                    case "6":
+                        validOption = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry.");
+                        Console.WriteLine();
+                        validOption = false;
+                        break;
+                }
             }
         }
 
         private static void CreateContact()
         {
-            Console.WriteLine("\t1 - Press 1 to enter the contact's details");
-            Console.WriteLine("\t2 - Press 2 to insert a contact with the default data");
-            Console.WriteLine("\t3 - Press 3 to back to the main menu");
+            bool validOption = false;
 
-            string optionNo = Console.ReadLine();
-            switch (optionNo)
+            while (!validOption)
             {
-                case "1":
-                    Console.WriteLine("Contact's details:");
-                    Console.Write("\nFirst Name:");
-                    string firstName = Console.ReadLine();
-                    Console.Write("\nLast Name:");
-                    string lastName = Console.ReadLine();
-                    Console.Write("\nPhone Number:");
-                    string phoneNumber = Console.ReadLine();
-                    Console.Write("\nE-Mail Address:");
-                    string emailAddress = Console.ReadLine();
-                    try
-                    {
-                        int createdContactId = ContactController.CreateContact(firstName, lastName, phoneNumber, emailAddress);
-                        Console.WriteLine("Contact has been added successfully, ContactID = " + createdContactId);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error while trying to create new Contact");
-                        Console.WriteLine(ex.Message);
-                    }
-                    ChooseOption();
-                    break;
-                case "2":
-
-                    string defaultFirstName = "John";
-                    string defaultLastName = "Smith";
-                    string defaultPhoneNumber = "000-0000-0000";
-                    string defaultEMailAddress = "john.smith";
-                    try
-                    {
-                        int createdContactId = ContactController.CreateContact(defaultFirstName, defaultLastName, defaultPhoneNumber, defaultEMailAddress);
-                        Console.WriteLine("Contact with the default data has been added successfully, ContactID = " + createdContactId);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error while trying to create new Contact with the default");
-                        Console.WriteLine(ex.Message);
-                    }
-                    ChooseOption();
-                    break;
-                case "3":
-                    ChooseOption();
-                    break;
-                default:
-                    ChooseOption();
-                    break;
+                Console.WriteLine("Choose an Option:");
+                Console.WriteLine("\t1 - Press 1 to enter the contact's details");
+                Console.WriteLine("\t2 - Press 2 to insert a contact with the default data");
+                Console.WriteLine("\t3 - Press 3 to back to the main menu");
+                Console.Write("Your choice: ");
+                string optionNo = Console.ReadLine();
+                Console.WriteLine();
+                switch (optionNo)
+                {
+                    case "1":
+                        validOption = true;
+                        Console.WriteLine("Contact's details:");
+                        Console.Write("First Name: ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Last Name: ");
+                        string lastName = Console.ReadLine();
+                        Console.Write("Phone Number: ");
+                        string phoneNumber = Console.ReadLine();
+                        Console.Write("E-Mail Address: ");
+                        string emailAddress = Console.ReadLine();
+                        Console.WriteLine();
+                        try
+                        {
+                            int createdContactId = ContactController.CreateContact(firstName, lastName, phoneNumber, emailAddress);
+                            Console.WriteLine("Contact has been added successfully, ContactID = " + createdContactId);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error while trying to create new Contact: {ex.Message}");
+                        }
+                        ChooseOption();
+                        break;
+                    case "2":
+                        validOption = true;
+                        string defaultFirstName = "John";
+                        string defaultLastName = "Smith";
+                        string defaultPhoneNumber = "000-000-0000";
+                        string defaultEMailAddress = "john.smith@yahoo.com";
+                        try
+                        {
+                            int createdContactId = ContactController.CreateContact(defaultFirstName, defaultLastName, defaultPhoneNumber, defaultEMailAddress);
+                            Console.WriteLine("Contact with the default data has been added successfully, ContactID = " + createdContactId);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error while trying to create new Contact: {ex.Message}");
+                        }
+                        ChooseOption();
+                        break;
+                    case "3":
+                        validOption = true;
+                        ChooseOption();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry.");
+                        Console.WriteLine();
+                        validOption = false;
+                        break;
+                }
             }
         }
         private static void UpdateContact()
         {
-            Console.WriteLine("\t1 - Press 1 to enter the contact's details");
-            Console.WriteLine("\t2 - Press 2 to back to the main menu");
+            bool validOption = false;
 
-            string optionNo = Console.ReadLine();
-            switch (optionNo)
+            while (!validOption)
             {
-                case "1":
-                    Console.WriteLine("Contact's details:");
-                    Console.Write("\nContact ID:");
-                    int contactID = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("\nFirst Name:");
-                    string firstName = Console.ReadLine();
-                    Console.Write("\nLast Name:");
-                    string lastName = Console.ReadLine();
-                    Console.Write("\nPhone Number:");
-                    string phoneNumber = Console.ReadLine();
-                    Console.Write("\nE-Mail Address:");
-                    string emailAddress = Console.ReadLine();
-                    try
-                    {
-                        int updatedContactId = ContactController.UpdateContact(contactID, firstName, lastName, phoneNumber, emailAddress);
-                        Console.WriteLine("Contact has been updated successfully, ContactID = " + updatedContactId);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error while trying to update Contact");
-                        Console.WriteLine(ex.Message);
-                    }
-                    ChooseOption();
-                    break;
-                case "2":
-                    ChooseOption();
-                    break;
-                default:
-                    ChooseOption();
-                    break;
+                Console.WriteLine("Choose an Option:");
+                Console.WriteLine("\t1 - Press 1 to enter the contact's details");
+                Console.WriteLine("\t2 - Press 2 to back to the main menu");
+                Console.Write("Your choice: ");
+                string optionNo = Console.ReadLine();
+                Console.WriteLine();
+                switch (optionNo)
+                {
+                    case "1":
+                        validOption = true;
+                        Console.WriteLine("Contact's details:");
+                        Console.Write("Contact ID: ");
+                        int contactID = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("First Name: ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Last Name: ");
+                        string lastName = Console.ReadLine();
+                        Console.Write("Phone Number: ");
+                        string phoneNumber = Console.ReadLine();
+                        Console.Write("E-Mail Address: ");
+                        string emailAddress = Console.ReadLine();
+                        Console.WriteLine();
+                        try
+                        {
+                            int updatedContactId = ContactController.UpdateContact(contactID, firstName, lastName, phoneNumber, emailAddress);
+                            Console.WriteLine("Contact has been updated successfully, ContactID = " + updatedContactId);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error while trying to update Contact: {ex.Message}");
+                        }
+                        ChooseOption();
+                        break;
+                    case "2":
+                        validOption = true;
+                        ChooseOption();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry.");
+                        Console.WriteLine();
+                        validOption = false;
+                        break;
+                }
             }
         }
         private static void DeleteContact()
         {
-            Console.WriteLine("\t1 - Press 1 to enter the contact's details");
-            Console.WriteLine("\t2 - Press 2 to back to the main menu");
+            bool validOption = false;
 
-            string optionNo = Console.ReadLine();
-            switch (optionNo)
+            while (!validOption)
             {
-                case "1":
-                    Console.Write("\nEnter Contact ID:");
-                    int contactID = Convert.ToInt32(Console.ReadLine());
-                    try
-                    {
-                        bool deleteResult = ContactController.DeleteContact(contactID);
-                        Console.WriteLine("Contact has been deleted successfully!");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error while trying to update Contact");
-                        Console.WriteLine(ex.Message);
-                    }
+                Console.WriteLine("Choose an Option:");
+                Console.WriteLine("\t1 - Press 1 to enter the contact's details");
+                Console.WriteLine("\t2 - Press 2 to back to the main menu");
+                Console.Write("Your choice: ");
+                string optionNo = Console.ReadLine();
+                Console.WriteLine();
+                switch (optionNo)
+                {
+                    case "1":
+                        validOption = true;
+                        Console.Write("Enter Contact ID: ");
+                        int contactID = Convert.ToInt32(Console.ReadLine());
+                        try
+                        {
+                            bool deleteResult = ContactController.DeleteContact(contactID);
+                            Console.WriteLine("Contact has been deleted successfully!");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error while trying to update Contact: {ex.Message}");
+                        }
 
-                    ChooseOption();
-                    break;
-                case "2":
-                    ChooseOption();
-                    break;
-                default:
-                    ChooseOption();
-                    break;
+                        ChooseOption();
+                        break;
+                    case "2":
+                        validOption = true;
+                        ChooseOption();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry.");
+                        Console.WriteLine();
+                        validOption = false;
+                        break;
+                }
             }
         }
 
@@ -195,8 +242,7 @@ namespace SQLFundamentals.UI.CRUDConsole
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error while trying to retrieving Contacts");
-                Console.WriteLine(ex.Message);
+                Console.WriteLine($"Error while trying to retrieve Contacts: {ex.Message}");
             }
             ChooseOption();
         }
@@ -205,7 +251,7 @@ namespace SQLFundamentals.UI.CRUDConsole
         {
             try
             {
-                Console.Write("\nEnter Contact ID:");
+                Console.Write("\nEnter Contact ID: ");
                 int contactID = Convert.ToInt32(Console.ReadLine());
                 ContactModel contactModel = ContactController.GetContactByID(contactID);
                 Console.WriteLine(
@@ -221,8 +267,7 @@ namespace SQLFundamentals.UI.CRUDConsole
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error while trying to retrieving a Contact");
-                Console.WriteLine(ex.Message);
+                Console.WriteLine($"Error while trying to retrieve a Contact: {ex.Message}");
             }
 
             ChooseOption();
